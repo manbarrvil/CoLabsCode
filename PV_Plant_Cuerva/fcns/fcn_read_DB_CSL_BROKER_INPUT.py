89c47992c8933c29a9cd781361c1b80c64cb6c70
manbarrvil <manbarrvil@gmail.com>
2025-03-20 11:27:19 +0100

    json pv0102 modbus

--- PV_Plant_Cuerva/fcns/fcn_read_DB_CSL_BROKER_INPUT.py
+++ PV_Plant_Cuerva/fcns/fcn_read_DB_CSL_BROKER_INPUT.py
@@ -11,7 +11,8 @@ def read_DB_CSL_BROKER_INPUT(db_host,db_port,db_user,db_password,db_name,conexio
     #     dbname=db_name
     # )
     cursor=conexion.cursor()
-    cursor.execute("select Date_Time, POI_Va,POI_P,POI_Q,CT1_Vab,CT1_P,CT1_Q,CT2_Vab,CT2_P,CT2_Q from DB_CSL_BROKER_INPUT")
+    # cursor.execute("select Date_Time, POI_Va,POI_P,POI_Q,CT1_Vab,CT1_P,CT1_Q,CT2_Vab,CT2_P,CT2_Q from DB_CSL_BROKER_INPUT")
+    cursor.execute("select Date_Time, POI_Va,POI_P,POI_Q,CT1_Vab,CT1_P,CT1_Q,CT2_Vab,CT2_P,CT2_Q FROM DB_CSL_BROKER_INPUT ORDER BY Date_Time DESC LIMIT 1")
     #for data in cursor:
     data = cursor.fetchone()
     while data:
